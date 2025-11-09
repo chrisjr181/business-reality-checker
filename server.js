171558a77d0017b871199d19738b670e773df73f
Chris Rhone <chrisjr181@gmail.com>
2025-11-09 10:43:58 -0500

    Initial commit

--- server.js
+++ server.js
@@ -2,7 +2,8 @@ const { v4: uuidv4 } = require('uuid');
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+const cors = require('cors');
+app.use(cors({ origin: ['https://smallbizblueprint.org'] }));
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
